6667c9b4c08b657077a269f60b92f1bac8777592
Carlos Torrejon <carlostc22@gmail.com>
2023-11-18 20:39:29 +0100

    Testing

--- Marvel/MarvelTests/MarvelTests.swift
+++ Marvel/MarvelTests/MarvelTests.swift
@@ -6,30 +6,30 @@
 //
 
 import XCTest
+@testable import Marvel
 
 final class MarvelTests: XCTestCase {
+    
+    var viewModel: CharacterListViewModel!
 
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+    @MainActor override func setUpWithError() throws {
+        super.setUp()
+        viewModel = CharacterListViewModel()
     }
 
     override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        viewModel = nil
+        super.tearDown()
     }
 
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-        // Any test you write for XCTest can be annotated as throws and async.
-        // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
-        // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
-    }
-
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        measure {
-            // Put the code you want to measure the time of here.
+    @MainActor func testCharacterLoading() throws {
+        let expectation = self.expectation(description: "CharacterLoading")
+        viewModel.loadCharacters()
+        DispatchQueue.main.asyncAfter(deadline: .now() + 5) {
+            expectation.fulfill()
         }
+        waitForExpectations(timeout: 5, handler: nil)
+        XCTAssertFalse(viewModel.characters.isEmpty, "No characters loaded")
     }
 
 }
